554549125ae34a2141eba56cc7bb1ba70188c873
Gonzalo Winazki <gwinazki@eljumillano.com.ar>
2026-04-16 18:35:18 -0300

    depl

--- vue-nails/vite.config.js
+++ vue-nails/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.250:8094',
+        changeOrigin: true,
+      },
+    },
+  },
 })
